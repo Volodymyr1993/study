3cd70e5de680fa229c198fb0268c9c2a04f0393f
vtkachenko <vtkachenko@llnw.com>
2024-04-30 16:18:02 +0300

    remove folders

--- playwright_parsing/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from main import PageElement, CalendarPage
-from playwright.sync_api import Playwright, sync_playwright, expect, Page
-
-
-@pytest.fixture
-def calendar(page: Page):
-    calendar = CalendarPage(page)
-    yield page
