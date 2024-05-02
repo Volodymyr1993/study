b96f926261e8af4a1a24601d0ee364da843c6516
vtkachenko <vtkachenko@llnw.com>
2024-05-02 14:10:17 +0300

    new test

--- rozenka/tests/test_goto_python_page.py
+++ rozenka/tests/test_goto_python_page.py
@@ -14,3 +14,8 @@ def test_second_test(python_page):
 def test_third_test(python_page):
     python_page.get_started_button.click()
     assert python_page.writing_tests.is_visible(), "Installation is not sisible"
+
+
+def test_tasdhird_test(python_page):
+    python_page.get_started_button.click()
+    assert python_page.writing_tests.is_visible(), "Installation is not sisible"
