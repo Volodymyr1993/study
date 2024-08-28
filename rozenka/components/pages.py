a03dd09b14e0de14fb4aa218fda91015aa965fde
vtkachenko <vtkachenko@llnw.com>
2024-08-28 21:44:55 +0300

    experiment 17

--- rozenka/components/pages.py
+++ rozenka/components/pages.py
@@ -2,6 +2,7 @@ from rozenka.components.base_page import BasePage
 from rozenka.components.components import PythonMixin, CommonMixin
 from playwright.sync_api import Page, TimeoutError, expect
 
+
 class PythonPage(CommonMixin, PythonMixin, BasePage):
     pass
 
